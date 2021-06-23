7bfd6d8a61a4eb7aa103b3110116400c5c172b84
Aminul islam <aminulislam565688@gmail.com>
2021-06-23 15:40:29 +0600

    i made the home page for Blogger Website

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,164 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Popover, Transition } from '@headlessui/react'
+import { XIcon } from '@heroicons/react/outline'
+import { Fragment } from 'react'
+import BlogCart from '../../component/BlogCart/BlogCart'
+import heroBG1 from '../../images/hero-bg-1.jpg'
+
+const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'Features', href: '#' },
+    { name: 'Marketplace', href: '#' },
+    { name: 'Company', href: '#' },
+]
+
+export default function Home() {
+    return (
+        <>
+            <div className="relative bg-white overflow-hidden">
+                <div className="max-w-7xl mx-auto">
+                    <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                        <svg
+                            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                            fill="currentColor"
+                            viewBox="0 0 100 100"
+                            preserveAspectRatio="none"
+                            aria-hidden="true"
+                        >
+                            <polygon points="50,0 100,0 50,100 0,100" />
+                        </svg>
+
+                        <Popover>
+                            {({ open }) => (
+                                <>
+                                    <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                                        <nav
+                                            className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                                            aria-label="Global"
+                                        >
+                                            <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                                                <div className="flex items-center justify-between w-full md:w-auto">
+
+                                                </div>
+                                            </div>
+                                            <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+
+                                            </div>
+                                        </nav>
+                                    </div>
+
+                                    <Transition
+                                        show={open}
+                                        as={Fragment}
+                                        enter="duration-150 ease-out"
+                                        enterFrom="opacity-0 scale-95"
+                                        enterTo="opacity-100 scale-100"
+                                        leave="duration-100 ease-in"
+                                        leaveFrom="opacity-100 scale-100"
+                                        leaveTo="opacity-0 scale-95"
+                                    >
+                                        <Popover.Panel
+                                            focus
+                                            static
+                                            className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                                        >
+                                            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                                <div className="px-5 pt-4 flex items-center justify-between">
+                                                    <div>
+                                                        <img
+                                                            className="h-8 w-auto"
+                                                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                                            alt=""
+                                                        />
+                                                    </div>
+                                                    <div className="-mr-2">
+                                                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                                            <span className="sr-only">Close main menu</span>
+                                                            <XIcon className="h-6 w-6" aria-hidden="true" />
+                                                        </Popover.Button>
+                                                    </div>
+                                                </div>
+                                                <div className="px-2 pt-2 pb-3 space-y-1">
+                                                    {navigation.map((item) => (
+                                                        <a
+                                                            key={item.name}
+                                                            href={item.href}
+                                                            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                                                        >
+                                                            {item.name}
+                                                        </a>
+                                                    ))}
+                                                </div>
+                                                <a
+                                                    href="#"
+                                                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                                                >
+                                                    Log in
+                                                </a>
+                                            </div>
+                                        </Popover.Panel>
+                                    </Transition>
+                                </>
+                            )}
+                        </Popover>
+
+                        <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                            <div className="sm:text-center lg:text-left">
+                                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                                    <span className="block xl:inline">Data to enrich your</span>{' '}
+                                    <span className="block text-indigo-600 xl:inline">online business</span>
+                                </h1>
+                                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+                                    fugiat veniam occaecat fugiat aliqua.
+                                </p>
+                                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                    <div className="rounded-md shadow">
+                                        <a
+                                            href="#"
+                                            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                                        >
+                                            Get started
+                                        </a>
+                                    </div>
+                                    <div className="mt-3 sm:mt-0 sm:ml-3">
+                                        <a
+                                            href="#"
+                                            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                                        >
+                                            Live demo
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+                <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                    <img
+                        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+                        src={heroBG1}
+                        alt=""
+                    />
+                </div>
+            </div>
+            <section class="text-gray-600 body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
+                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                        </svg>
+                        <p class="leading-relaxed text-lg">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware. Man bun next level coloring book skateboard four loko knausgaard. Kitsch keffiyeh master cleanse direct trade indigo juice before they sold out gentrify plaid gastropub normcore XOXO 90's pickled cindigo jean shorts. Slow-carb next level shoindigoitch ethical authentic, yr scenester sriracha forage franzen organic drinking vinegar.</p>
+                        <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+                        <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
+                        <p class="text-gray-500">Senior Product Designer</p>
+                    </div>
+                </div>
+            </section>
+            <div className='grid grid-cols-2 gap-4'>
+                <BlogCart></BlogCart>
+                <BlogCart></BlogCart>
+            </div>
+        </>
+    )
+}
